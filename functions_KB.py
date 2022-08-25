ad280c019a88dd0aaa0d3838dcc3acb843637dd1
suzinoh <100442392+suzinoh@users.noreply.github.com>
2022-08-25 11:18:31 -0500

    Add files via upload

---
+++ functions_KB.py
@@ -0,0 +1,164 @@
+from datetime import date
+from pathlib import Path
+"""knolwedge base related functions stored here"""
+
+"""po is shortened term for possible options. gets populated by def populate with lists in file """
+po = {
+    "category": [],
+    "property": [],
+    "affordance": []
+}
+
+
+def populate():
+    # populating categories
+    f = open("categories", "r")
+    lines = f.readlines()
+    for each in lines:
+        each=each.replace("\n", "")
+        if each not in po["category"]:
+            po["category"].append(each)
+    print(po["category"])
+
+    # populating affordance
+    f = open("affordance", "r")
+    lines = f.readlines()
+    for each in lines:
+        each = each.replace("\n", "")
+        if each not in po["affordance"]:
+            po["affordance"].append(each)
+
+def append_choice(element, list):
+    command = input("new element: " , element, "detected. ","\n","TYPE Y if adding to list:")
+    if command == "Y":
+        list.append(element)
+        #TODO: before closing the file, check if the list matches the original content. if not, append
+
+
+def split_kb(ordered, key_list):
+    dictionary = open("dictionary_1", "w")
+    for i in range(0, 199):
+        dictionary.write(key_list[i])
+        dictionary.write("\n")
+        dictionary.write(ordered[key_list[i]])
+        dictionary.write("\n")
+    dictionary = open("dictionary_2", "w")
+    for i in range(200, 399):
+        dictionary.write(key_list[i])
+        dictionary.write("\n")
+        dictionary.write(ordered[key_list[i]])
+        dictionary.write("\n")
+    dictionary = open("dictionary_3", "w")
+    for i in range(400, 599):
+        dictionary.write(key_list[i])
+        dictionary.write("\n")
+        dictionary.write(ordered[key_list[i]])
+        dictionary.write("\n")
+    dictionary = open("dictionary_4", "w")
+    for i in range(600, 671):
+        dictionary.write(key_list[i])
+        dictionary.write("\n")
+        dictionary.write(ordered[key_list[i]])
+        dictionary.write("\n")
+
+def check_existing_category(category):
+    for item in  po["category"]:
+        if category == item:
+            return True
+    else:
+        return False
+
+def check_existing_affordance(rest):
+    for item in po["affordance"]:
+        if rest == item:
+            return True
+    else:
+        return False
+
+def verify_affordance(input_afford):
+    """verifying the new inputted affordance if they exist in the possible option"""
+    counter = 0
+    for item in po["affordance"]:
+        if item.find(input_afford) != -1:
+            print("replacing the old Affordance with ", item)
+            return item
+    else:
+        return -1
+
+def perfecting_category(filename):
+    replace = ["is_a", "(", ")", " ", ".", ",", "\n"]
+    choice_list = po["category"]
+    f = open(filename, "r")
+    # getting today's date and adding to file name
+    today = date.today()
+    newfile = filename + "_perf_" + str(today)
+    path = Path(newfile)
+    flag = True
+    # uncomment when the file is all perfected
+    # if path.is_file():
+    #     print("file have been previously perfected")
+    #     return None
+    # else:
+    if flag is True:
+        f2 = open(newfile, "w")
+        lines = f.readlines()
+        word = ""
+        for each_line in lines:
+            if "is_a" not in each_line and "has_" not in each_line and "has_function" not in each_line and "has_affordance" not in each_line and "%definition" not in each_line and "%has_Affordance" not in each_line:
+                word = each_line.replace("\n", "")
+                word = word.lower()
+                replace.append(word)
+            if "%definition" in each_line:
+                definition = each_line
+            if "is_a" in each_line:
+                category = each_line
+                for item in replace:
+                    category = category.replace(item, '')
+                if check_existing_category(category) is False:
+                    print("---> incorrect category please reconfigure category to replace ", category, "for ", word)
+                    print(definition)
+                    print(choice_list)
+                    new_category = input("---> enter new category: ")
+                    if new_category not in choice_list:
+                        print("**new category** keeping the original category \n")
+
+                    else:
+                        each_line = "is_a(%s, %s).\n" % (word, new_category)
+            f2.write(each_line)
+def perfecting_affordance(filename):
+    """take in dictionary_#_perf, and replace the function part to affordance and fix the errors"""
+    replace = ["has_affordance", "has_Affordance", "has_accordance", "(", ")", " ", ".", ",", "\n"]
+    f = open(filename, "r")
+    today = str(date.today())
+    temp = filename+"_temp"+today
+    f2 = open(temp, "w")
+    for each_line in f:
+        # combining the function with affordance
+        if len(each_line) < 15:
+            word = each_line.replace("\n","")
+            word = word.lower()
+            replace.append(word)
+        if "%definition" in each_line:
+            definition = each_line
+        check = each_line[:12:]
+        rest = each_line[12::]
+        if check!="has_function":
+            f2.write(each_line)
+        else:
+            for item in replace:
+                rest = rest.replace(item, '')
+            if check_existing_affordance(rest) is False:
+                #TODO: must support the case where having affordance does not make sense... ex) superman. maybe with category
+                print("---> incorrect category please reconfigure Affordance to replace ", rest, " for ", word)
+                print(definition)
+                print(po["affordance"])
+                new_category = input("---> enter new Affordance: ")
+                verified = verify_affordance(new_category)
+                if verified != -1:
+                    new_affordance = verified
+                    rest = "(" + word + new_affordance + ")"
+                else:
+                    print("*** affordance not specified ***")
+            newline ="has_affordance"+rest+"\n"
+            f2.write(newline)
+    f2.close()
---
+++ main.py
@@ -0,0 +1,44 @@
+# TODO: import the files and organize by alphabetical ord
+import os
+
+import functions_KB
+import functions_KB as functions_KB
+# from data_structure import ObjectNode
+
+directory = 'C:/Users/suzin/OneDrive/Documents/WSU/Research/KnolwedgeBase'
+filelist = []
+for filename in os.scandir(directory):
+    if filename.is_file():
+        filelist.append(filename)
+ordered = {}
+key_list = []
+# reading the file using the file list name
+# TODO: have some check (amount of lines in the files accumulated is same length as total line outputted)
+for i in range(len(filelist)):
+    f = open(filelist[i], 'r')
+    lines = f.readlines()
+    for each_line in lines:
+        if "%word" in each_line:
+            word = each_line[6:-1:]
+            replace = ""
+            for character in word:
+                if (character != " ") and (character != ":"):
+                    replace = replace + character
+            word = replace
+        elif "%definition" in each_line:
+            definition = each_line
+        elif not len(each_line)<3:
+            definition = definition + each_line
+        else:
+            ordered[word] = definition
+            definition = ""
+            if word not in key_list:
+                key_list.append(word)
+#total length of the words are  672, splitting it by 100, use function_KB, split_kb if needed
+
+# TODO: need to make sure each dict item's description is valid, options should be limited
+functions_KB.populate()
+#functions_KB.perfecting_category("dictionary_1")
+functions_KB.perfecting_affordance("dictionary_1")
+
+# combine them into one file instead of separate files
